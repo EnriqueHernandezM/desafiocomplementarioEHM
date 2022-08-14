4d3256c85848e6dff2f2d85dcae5793dfda4d326
Enrique HM <enriquehmsb@gmail.com>
2022-08-13 20:50:26 -0500

    segunda entrega

--- pruebas.js
+++ pruebas.js
@@ -1,7 +1,25 @@
-//primer funcion
 function datosDeIngreso(nombre, edad, apellido) {
   let esp = " ";
-  nombre = prompt("Esta es una pagina solo para mayores de edad INGRESA tus datos para continuar Nombre ");
+  nombre = prompt("ingresa tu nombre");
+  apellido = prompt("igresa tu  apellido");
+  edad = parseInt(prompt("ingresa tu edad"));
+  while (edad <= 17) {
+    alert("no tienes la edad suficiente para navegar en este sitio");
+  }
+  if (edad >= 18) {
+    let resulta = "Hola" + esp + nombre + esp + apellido + esp + "tienes" + esp + edad + esp + "años" + esp + "bienvenido";
+    alert(resulta);
+  } else {
+    edad = 0;
+    while (edad == 0) {
+      alert("no llenaste los campos requeridos");
+    }
+  }
+}
+/primer funcion
+function datosDeIngreso(nombre, edad, apellido) {
+  let esp = " ";
+  nombre = prompt("ingresa tu nombre");
   apellido = prompt("igresa tu  apellido");
   edad = parseInt(prompt("ingresa tu edad"));
   while (edad <= 17) {
@@ -44,18 +62,21 @@ function calculadoraDePrecios(calcular, precio1, precio2) {
   }
 }
 //terminan funciones
-datosDeIngreso();
 let preguntaInicial = prompt("ingresa el dia en que quieres conocer nuestras promociones");
 if (preguntaInicial == "lunes") {
+  datosDeIngreso();
   alert("Los lunes tenemos 15% de descuento en vinos, tequila y agua mineral");
   calculadoraDePrecios();
 } else if (preguntaInicial == "martes") {
+  datosDeIngreso();
   alert("Los martes tenemos 10% de descuento en whiskey y MSI en tdc");
   calculadoraDePrecios();
 } else if (preguntaInicial == "viernes") {
+  datosDeIngreso();
   alert("Los viernes tenemos toda la tienda con 10% de descuento");
   calculadoraDePrecios();
 } else {
+  datosDeIngreso();
   alert("Lo sentimos por el momento no tenemos promociones para es dia");
 }
 let preguntaAgregarArticulos = prompt("DESEA AGGREGAR ALGUNO DE NUESTROS ARTICULOS 'si / no'");
